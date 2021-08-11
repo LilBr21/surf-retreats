1a09f93fc0da2d061d7338a7e5a0829aeb9fe30a
Halina <halina.syposz@gmail.com>
2021-08-11 13:53:33 +0200

    header added and minor style changes provided

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import Form from './components/Form';
 import './App.css';
 import {ReactComponent as Wave} from './wave.svg';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="container">
-      <div className="App">
-        <Form />
+    <div className="surfretreat">
+      <div className="header">
+        <Header />
       </div>
-      <div className="wave">
-        <Wave />
+      <div className="container">
+        <div className="App">
+          <Form />
+        </div>
+        <div className="wave">
+          <Wave />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+// import headerpic from '../headerpic.png'
+import './Header.styles.css';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="headerpic">
+            <h1 className="title">Surf Chasers</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Header;

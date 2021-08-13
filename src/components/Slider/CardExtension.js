2c68159d741f7538201ee8a67628ffb662c82334
Halina <halina.syposz@gmail.com>
2021-08-13 19:27:59 +0200

    card extension styles improved

--- src/components/Slider/CardExtension.js
+++ src/components/Slider/CardExtension.js
@@ -10,10 +10,10 @@ let portugalExtension = {
 
 let costaRicaExtension = {
     extname: 'Costa Rica',
-    extdesc: 'sth shit',
-    accomodation: 'sth',
-    prices: '$$$',
-    dates: '249458'
+    extdesc: "Experience Costa Rica's tropical chill vibe with us. We normally stay in the south part which guarantees the best and longest left waves we've ever surfed. Long sandy beaches are waiting for you.",
+    accomodation: 'In costa rica we stay in bungalows near the beach. That means that it takes about a minute walking to go surfing and there are beautiful sunsets to watch from our terrace. In Costa Rica we provide only shared accomodation (4 - 6 persons in one room).',
+    prices: '1050$',
+    dates: '22.11.2021 - 28.11.2021, 13.12.2021 - 19.12.2021, 21.03.2022 - 27.03.2022'
 }
 
 let panamaExtension = {
@@ -42,7 +42,10 @@ let extendedInfo = [
 const CardExtension = (props) => {
     return (
         <div className="extended-card">
-            <h2 className="extended-title">{extendedInfo[props.chosenplace].extname}</h2>
+            <div className="top-line">
+                <p className="closing-cross">+</p>
+                <h2 className="extended-title">{extendedInfo[props.chosenplace].extname}</h2>
+            </div>
             <h4>Basic info:</h4>
             <p className="basic-txt">{extendedInfo[props.chosenplace].extdesc}</p>
             <h4>Classes:</h4>
